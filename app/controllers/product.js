9bf0195dc744044f2429986d9b3018141a04ee5d
dayamll <dayamllerenacastro@gmail.com>
2018-02-26 03:37:12 -0500

    subiendo estructura de las vistas y funciones de paypal

--- app/controllers/product.js
+++
@@ -1,6 +0,0 @@
-// controller apunta a app
-// $scope -> variable intermediaria entre el js y el front
-app.controller('product',  ['$scope', 'productModel', function( $scope, productModel ) {
-	
-
-}]);
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const server = app.listen(3000, encender);
+const server = app.listen(4840, encender);
 
 function encender() {
   console.log('Servidor encendido');
